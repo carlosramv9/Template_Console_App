224de2b9dd16c73055a842da364ebb39566ee26e
Carlos Ramirez <carlos.ramv97@gmail.com>
2024-11-26 21:21:59 -0700

    Agregar archivos de proyecto.

---
+++ Task_FileMigration/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Task_FileMigration;
+
+var builder = new ConfigurationBuilder();
+
+builder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+var logDirectory = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+var logName = Path.Combine(logDirectory, $"log_{DateTime.Now:yyyyMMddHHmmss}.log");
+
+if (!Directory.Exists(logDirectory))
+{
+    Directory.CreateDirectory(logDirectory);
+}
+
+Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .WriteTo.Console()
+                .WriteTo.File(logName)
+                .CreateLogger();
+
+Log.Logger.Information("starting serilog in a console app...");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) => {
+        // here we can build our services...
+        services.AddSingleton<IConfiguration>(builder.Build());
+        services.AddSingleton<ILogger>(Log.Logger);
+    })
+.UseSerilog()
+.Build();
+
+var startup = host.Services.GetRequiredService<Startup>();
+
+startup.Run();
---
+++ Task_FileMigration/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_FileMigration
+{
+    public class Startup
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
